132a910759242572075390b485e6da3665a464d0
Andrew Matthews <apmatthe@ncsu.edu>
2015-01-26 15:45:54 -0500

    error handling

--- ncstate-social-sdk.php
+++ ncstate-social-sdk.php
@@ -1,8 +1,13 @@
 <?php
 
 require "vendor/autoload.php";
-require "config.php";
 
+if (file_exists('config.php')) {
+  require "config.php";
+} else {
+  error_log("Missing config.php. Copy the sample file and input your credentials", 0);
+  exit(1);
+}
 
 if($load_apis['facebook']) include "facebook.php" ;
 if($load_apis['twitter'])include "twitter.php";
