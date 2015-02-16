b2e750ac7788c54b715f16049501998d803be01e
Andrew Matthews <apmatthe@ncsu.edu>
2015-02-16 13:02:04 -0500

    use dirname for loading files regardless of included directory. Fixes #1

--- ncstate-social-sdk.php
+++ ncstate-social-sdk.php
@@ -1,15 +1,15 @@
 <?php
 
-require "vendor/autoload.php";
+require(dirname(__FILE__) . '/vendor/autoload.php');
 
 if (file_exists('config.php')) {
-  require "config.php";
+  require(dirname(__FILE__) . '/config.php');
 } else {
   error_log("Missing config.php. Copy the sample file and input your credentials", 0);
   exit(1);
 }
 
-if($load_apis['facebook']) include "facebook.php" ;
-if($load_apis['twitter'])include "twitter.php";
-if($load_apis['instagram'])include "instagram.php";
+if($load_apis['facebook']) include_once(dirname(__FILE__) . '/facebook.php');
+if($load_apis['twitter']) include_once(dirname(__FILE__) . '/twitter.php');
+if($load_apis['instagram']) include_once(dirname(__FILE__) . '/instagram.php');
 
