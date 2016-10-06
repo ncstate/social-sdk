83750a8af923778258e7d8c87fd164d6feee423b
Johnathan Smith <jrsmit24@ncsu.edu>
2016-10-06 14:31:38 -0400

    moved sorting. changed != false on sort to !empty() to target null and blank strings too.

--- flickr.php
+++ flickr.php
@@ -1,61 +1,5 @@
 <?php
 
-/* Sorting Mechanism for Flickr & Such */
-if (!class_exists('goodSorter')) {
-    class goodSorter
-    {
-
-        public $sort_fields;
-        public $backwards = false;
-        public $numeric = false;
-
-        public function sort()
-        {
-            $args = func_get_args();
-            $array = $args[0];
-            if (!$array) {
-                return array();
-            }
-
-            $this->sort_fields = array_slice($args, 1);
-            if (!$this->sort_fields) {
-                return $array();
-            }
-
-            if ($this->numeric) {
-                usort($array, array($this, 'numericCompare'));
-            } else {
-                usort($array, array($this, 'stringCompare'));
-            }
-            return $array;
-        }
-
-        public function numericCompare($a, $b)
-        {
-            foreach ($this->sort_fields as $sort_field) {
-                if ($a[$sort_field] == $b[$sort_field]) {
-                    continue;
-                }
-                return ($a[$sort_field] < $b[$sort_field]) ? ($this->backwards ? 1 : -1) : ($this->backwards ? -1 : 1);
-            }
-            return 0;
-        }
-
-        public function stringCompare($a, $b)
-        {
-            foreach ($this->sort_fields as $sort_field) {
-                $cmp_result = strcasecmp($a[$sort_field], $b[$sort_field]);
-                if ($cmp_result == 0) {
-                    continue;
-                }
-
-                return ($this->backwards ? -$cmp_result : $cmp_result);
-            }
-            return 0;
-        }
-    }
-}
-
 /**
  * Retrieves Flickr Photos sets from Album ID.
  *
@@ -73,7 +17,7 @@ function getFlickr($flickr_id, $num = 8, $sort = false)
 
     $the_photos = $flickr_photos['photoset']['photo'];
 
-    if ($sort != false) {
+    if (!empty($sort)) {
 
         $sorter = new goodSorter();
 
--- ncstate-social-sdk.php
+++ ncstate-social-sdk.php
@@ -1,21 +1,86 @@
 <?php
 
-define( 'NCSTATE_SOCIAL_PATH', dirname(__FILE__) . '/' );
+define('NCSTATE_SOCIAL_PATH', dirname(__FILE__) . '/');
 
-require_once( NCSTATE_SOCIAL_PATH . 'vendor/autoload.php' );
+require_once NCSTATE_SOCIAL_PATH . 'vendor/autoload.php';
 
-if ( defined('ABSPATH') && function_exists('ot_get_option') ) {
-	require_once( NCSTATE_SOCIAL_PATH . 'wp-config.php' );
-} elseif ( file_exists( NCSTATE_SOCIAL_PATH . 'config.php' ) ) {
-  require_once( NCSTATE_SOCIAL_PATH . 'config.php' );
+if (defined('ABSPATH') && function_exists('ot_get_option')) {
+    require_once NCSTATE_SOCIAL_PATH . 'wp-config.php';
+} elseif (file_exists(NCSTATE_SOCIAL_PATH . 'config.php')) {
+    require_once NCSTATE_SOCIAL_PATH . 'config.php';
 } else {
-  error_log("Missing config.php. Copy the sample file and input your credentials", 0);
-  exit(1);
+    error_log("Missing config.php. Copy the sample file and input your credentials", 0);
+    exit(1);
 }
 
+/* Sorting Mechanism for Flickr & Such */
+if (!class_exists('goodSorter')) {
+    class goodSorter
+    {
 
+        public $sort_fields;
+        public $backwards = false;
+        public $numeric = false;
 
-if($load_apis['facebook']) include_once( NCSTATE_SOCIAL_PATH . 'facebook.php');
-if($load_apis['twitter']) include_once( NCSTATE_SOCIAL_PATH . 'twitter.php');
-if($load_apis['instagram']) include_once( NCSTATE_SOCIAL_PATH . 'instagram.php');
-if($load_apis['flickr']) include_once( NCSTATE_SOCIAL_PATH . 'flickr.php');
+        public function sort()
+        {
+            $args = func_get_args();
+            $array = $args[0];
+            if (!$array) {
+                return array();
+            }
+
+            $this->sort_fields = array_slice($args, 1);
+            if (!$this->sort_fields) {
+                return $array();
+            }
+
+            if ($this->numeric) {
+                usort($array, array($this, 'numericCompare'));
+            } else {
+                usort($array, array($this, 'stringCompare'));
+            }
+            return $array;
+        }
+
+        public function numericCompare($a, $b)
+        {
+            foreach ($this->sort_fields as $sort_field) {
+                if ($a[$sort_field] == $b[$sort_field]) {
+                    continue;
+                }
+                return ($a[$sort_field] < $b[$sort_field]) ? ($this->backwards ? 1 : -1) : ($this->backwards ? -1 : 1);
+            }
+            return 0;
+        }
+
+        public function stringCompare($a, $b)
+        {
+            foreach ($this->sort_fields as $sort_field) {
+                $cmp_result = strcasecmp($a[$sort_field], $b[$sort_field]);
+                if ($cmp_result == 0) {
+                    continue;
+                }
+
+                return ($this->backwards ? -$cmp_result : $cmp_result);
+            }
+            return 0;
+        }
+    }
+}
+
+if ($load_apis['facebook']) {
+    include_once NCSTATE_SOCIAL_PATH . 'facebook.php';
+}
+
+if ($load_apis['twitter']) {
+    include_once NCSTATE_SOCIAL_PATH . 'twitter.php';
+}
+
+if ($load_apis['instagram']) {
+    include_once NCSTATE_SOCIAL_PATH . 'instagram.php';
+}
+
+if ($load_apis['flickr']) {
+    include_once NCSTATE_SOCIAL_PATH . 'flickr.php';
+}
